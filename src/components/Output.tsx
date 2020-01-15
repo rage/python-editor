36dff2fe92f92b01da0f84785094921f4c5d7954
Krista Lindholm <krista.lindholm@gmail.com>
2020-01-15 17:14:38 +0200

    Improve output styling

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,37 +1,64 @@
 import React from "react"
+import styled from "styled-components"
+import { Button, Paper, Grid, Typography } from "@material-ui/core"
 
 type OutputProps = {
   outputText: string
   clearOutput: () => void
 }
 
+const OutputBox = styled(Paper)`
+  && {
+    height: 210px;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+  }
+`
+
+const OutputTitleBox = styled(Grid)`
+  background-color: #2196f3;
+  color: white;
+  padding: 10px;
+  border-radius: 3px 3px 0 0;
+  box-shadow: inherit;
+`
+
+const OutputTitle = styled(Typography)`
+  && {
+    font-size: 1.62rem;
+    display: inline-block;
+  }
+`
+
+const FloatedButton = styled(Button)`
+  float: right;
+  display: inline-block;
+`
+
+const StyledOutput = styled(Grid)`
+  padding: 10px;
+  height: 140px;
+  overflow: auto;
+  white-space: pre-wrap;
+`
+
 const Output: React.FunctionComponent<OutputProps> = props => {
   const { outputText, clearOutput } = props
   if (!outputText || outputText.length === 0) return null
 
-  const outputContainerStyle = {
-    width: "100%",
-    height: "200px",
-    border: "solid 1px #dcdcdc",
-    backgroundColor: "white",
-    position: "absolute" as "absolute",
-    bottom: 0,
-  }
-
-  const outputStyle = {
-    overflow: "auto",
-    whiteSpace: "pre-wrap" as "pre-wrap",
-    height: "140px",
-  }
-
   return (
-    <div style={outputContainerStyle}>
-      <h3 style={{ display: "inline-block" }}>Output</h3>
-      <button onClick={clearOutput} style={{ float: "right" }}>
-        Close
-      </button>
-      <div style={outputStyle}>{props.outputText}</div>
-    </div>
+    <OutputBox>
+      <Grid container direction="column">
+        <OutputTitleBox item>
+          <OutputTitle>Output</OutputTitle>
+          <FloatedButton onClick={clearOutput} variant="contained">
+            Close
+          </FloatedButton>
+        </OutputTitleBox>
+        <StyledOutput item>{props.outputText}</StyledOutput>
+      </Grid>
+    </OutputBox>
   )
 }
 
