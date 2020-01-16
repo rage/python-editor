f35ae3eb926b5b2272e6595f2e9088857d313137
Juha Myllari <jmyllari@gmail.com>
2020-01-16 16:18:05 +0200

    Initialize quiz with file contents

--- src/components/QuizLoader.tsx
+++ src/components/QuizLoader.tsx
@@ -3,19 +3,23 @@ import JSZip from "jszip"
 import JSZipUtils from "jszip-utils"
 import axios from "axios"
 import fs from "fs"
+import { Quiz } from "."
 
 type QuizLoaderProps = {
   url: string
   token: string
 }
 
+/*  Loads the quiz from the server. Then returns a Quiz component
+    with the initial editor text set to the contents of the first
+    file whose path contains "/src/__main__.py".
+*/
 const QuizLoader: React.FunctionComponent<QuizLoaderProps> = ({
   url,
   token,
 }) => {
   const [text, setText] = useState("Initial text")
   useEffect(() => {
-    console.log("This is the effect")
     axios
       .request({
         responseType: "arraybuffer",
@@ -26,23 +30,18 @@ const QuizLoader: React.FunctionComponent<QuizLoaderProps> = ({
         },
       })
       .then(res => {
-        setText("Fetched " + res.data)
-        console.log(res)
         const zip = new JSZip()
         return zip.loadAsync(res.data)
       })
-      .then(zip => {
-        console.log(
-          "These should be the files contained in our zip: " +
-            JSON.stringify(zip.files),
-        )
+      .then(zip => zip.file(/src\/__main__.py/)[0].async("string"))
+      .then(str => {
+        setText(str)
       })
   }, [])
 
   return (
     <>
-      <p>Hello world, this is your QuizLoader!</p>
-      <p>{text}</p>
+      <Quiz editorInitialValue={text} />
     </>
   )
 }
