971ada3e049b57c5385487ba94aa999bc55c33a0
Juha Myllari <jmyllari@gmail.com>
2020-02-05 15:55:14 +0200

    Use 'find' instead of 'within'

--- example/cypress/integration/home_page_spec.js
+++ example/cypress/integration/home_page_spec.js
@@ -5,19 +5,17 @@ describe("The Main Page", () => {
 
   it("has a url input field", () => {
     const inputUrl = "http://www.foo.bar"
-    cy.get("[data-cy=url-input]").within(el => {
-      cy.get("input")
-        .type(inputUrl)
-        .should("have.value", inputUrl)
-    })
+    cy.get("[data-cy=url-input]")
+      .find("input")
+      .type(inputUrl)
+      .should("have.value", inputUrl)
   })
 
   it("has a user token input field", () => {
     const inputToken = "123abc000"
-    cy.get("[data-cy=token-input]").within(el => {
-      cy.get("input")
-        .type(inputUrl)
-        .should("have.value", inputToken)
-    })
+    cy.get("[data-cy=token-input]")
+      .find("input")
+      .type(inputToken)
+      .should("have.value", inputToken)
   })
 })
