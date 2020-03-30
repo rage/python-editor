0d7768309a87dc9c2e9622d8a251eca17c1944c6
Juha Myllari <jmyllari@gmail.com>
2020-03-30 16:32:41 +0300

    Remove commented-out import

--- src/components/PyEditor.tsx
+++ src/components/PyEditor.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState, useEffect } from "react"
-// import Editor from "@monaco-editor/react"
 import { ControlledEditor } from "@monaco-editor/react"
 import styled from "styled-components"
 import { Button } from "@material-ui/core"
