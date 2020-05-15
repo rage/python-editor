65b68ababb01190a1fbbcc9b1a58727f9e3a397f
Henrik Nygren <nygrenh@gmail.com>
2020-05-15 14:44:44 +0300

    Clean up exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import { Quiz } from "./components/Quiz"
-import { QuizLoader } from "./components/QuizLoader"
-import PyEditor from "./components/PyEditor"
-import Output from "./components/Output"
-import { useInput, useLocalStorage } from "./hooks/customHooks"
-export { Output, PyEditor, Quiz, QuizLoader, useInput, useLocalStorage }
+import { QuizLoader as ProgrammingExercise } from "./components/QuizLoader"
+
+export { ProgrammingExercise }
