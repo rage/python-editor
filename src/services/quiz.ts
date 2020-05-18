170c513cd27e83bf39ca01f6719d702a7a6c1aaa
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-05-18 11:55:03 +0300

    Prepare for release 0.5.1

--- src/services/quiz.ts
+++ src/services/quiz.ts
@@ -16,7 +16,7 @@ interface SubmitOptions {
 const getHeaders = (token: string) => ({
   Authorization: `Bearer ${token}`,
   client: "python_editor",
-  client_version: "0.5.0",
+  client_version: "0.5.1",
 })
 
 const getZippedQuiz = async (
