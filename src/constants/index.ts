a7d639cec3b257392b4e4f2810b0c4f5bfed1f63
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-05-28 15:41:55 +0300

    Release 0.7.0

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,10 @@
 import skulptMinJsSource from "./skulptMinJsSource"
 import skulptStdlibJsSource from "./skulptStdlibJsSource"
 import workerJsSource from "./workerJsSource"
+import packageJson from "../../package.json"
 
 const EDITOR_NAME = "moocfi_python_editor"
-const EDITOR_VERSION = "0.6.7"
+const EDITOR_VERSION = packageJson.version
 
 export {
   EDITOR_NAME,
