83ff6cd556788a9b56d66ce79d2c415b6c4e55a9
Jori Lampi <36279434+jolampi@users.noreply.github.com>
2020-06-12 13:53:35 +0300

    Hotfix 0.8.3, Regex bad and stupid

--- src/services/test_parsing.ts
+++ src/services/test_parsing.ts
@@ -1,5 +1,5 @@
 const removeFalseIsNotTrue = (input: string): string => {
-  const match = input.match(/^False is not true : (.+)/im)
+  const match = input.match(/^False is not true : ([\s\S]+)/i)
   return match ? match[1] : input
 }
 
