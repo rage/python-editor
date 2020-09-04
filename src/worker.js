75c64299df8ad6498d42a29a24c4ed52f4a8cc1a
Jori Lampi <jori.lampi@iki.fi>
2020-09-04 15:40:07 +0300

    Improve print alias, add useless input code

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,13 @@
 self.languagePluginUrl = "https://pyodide.cdn.iodide.io/"
 // importScripts("./pyodide/pyodide.js")
 
-let printBuffer = []
 let intervalId = null
 const batchSize = 50
 let running = false
 let intervalId2 = null
 
 // used to check if a control message "input_required" has been appended to buffer
-const checkForMsg = () => {
+const checkForMsg = (printBuffer) => {
   let msgObject = null
   if (typeof printBuffer[printBuffer.length - 1] === "object") {
     msgObject = printBuffer.pop()
@@ -22,15 +21,27 @@ const intervalManager = (runInterval) => {
   }
   if (runInterval) {
     intervalId = setInterval(() => {
+      let printBuffer = pyodide?.globals?._print_buffer ?? []
+
+      console.log("print", pyodide?.globals?._input_requested)
+      if (pyodide?.globals?._input_requested) {
+        printBuffer.push({ type: "input_required" })
+        pyodide.globals._input_requested = false
+      }
+
       if (printBuffer.length > 0) {
         let msgObject = null
         if (printBuffer.length <= batchSize) {
-          msgObject = checkForMsg()
+          msgObject = checkForMsg(printBuffer)
         }
         const batch = printBuffer.splice(0, batchSize)
+        console.log(batch)
+        console.log(printBuffer)
+        pyodide.globals._print_buffer = printBuffer
         postMessage({ type: "print_batch", msg: batch })
         if (msgObject) postMessage(msgObject)
       }
+
       if (!running && printBuffer.length === 0) {
         clearInterval(intervalId)
         postMessage({ type: "print_done" })
@@ -39,27 +50,39 @@ const intervalManager = (runInterval) => {
   }
 }
 
-let prevDate = null
-function outf(text) {
-  printBuffer.push(text)
-  const newDate = Date.now()
-  if (newDate - prevDate > 50) {
-    postMessage({
-      type: "print_batch",
-      msg: printBuffer.splice(0, batchSize),
-    })
-    prevDate = newDate
-  }
-}
-
 const printAlias = `
-import sys
+import sys, time
 
-output = []
+_print_buffer = []
+_input_requested = False
+_input_buffer = ["ras"]
 
-def print(data):
-    output.append(str(data))
-    sys.stdout.write(str(data))
+def print(*objects, sep=' ',  end='\\n'):
+    def _print(object, end):
+        global _print_buffer
+        _print_buffer += list(str(object) + end)
+
+    if len(objects) == 0:
+        _print("", end)
+        return
+
+    for object in objects[:-1]:
+        _print(object, sep)
+    _print(objects[-1], end)
+
+    # sys.stdout.write(str(objects))
+
+def input(prompt=None):
+    global _input_requested
+    global _input_buffer
+    print(prompt, end='')
+    #_input_requested = True
+    #while len(_input_buffer) == 0:
+    #    time.sleep(1)
+
+    ret = _input_buffer.join('')
+    _input_buffer = []
+    return ret
 `
 
 function run(code) {
@@ -77,10 +100,7 @@ ${code}
             .runPythonAsync(code)
             .then(() => {
               printBuffer.push()
-              console.log("running pyodide completed", pyodide.globals.output)
-              if (pyodide.globals.output) {
-                outf(pyodide.globals.output.join("\n"))
-              }
+              console.log("running pyodide completed")
               postMessage({ type: "ready" })
             })
             .catch((e) => {
@@ -100,7 +120,12 @@ ${code}
 
 self.onmessage = function (e) {
   const { type, msg } = e.data
-  if (type === "run") {
+  if (type === "input") {
+    console.log("input command")
+    if (pyodide.globals._input_requested) {
+      pyodide.globals._input_buffer = msg.split("")
+    }
+  } else if (type === "run") {
     console.log("run command")
     intervalManager(true)
     running = true
